1e39d70c92af798e0d33435fb2a18daab7f178e0
James Alexander <60295314+Jalexander01@users.noreply.github.com>
2021-06-08 10:58:12 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Here</h1>
+    <p>
+      Geckos are a group of usually small, usually nocturnal lizards. They are
+      found on every continent except Australia.
+    </p>
+
+    <p>
+      Some species live in houses where they hunt insects attracted by
+      artificial light.
+    </p>
+    <p>Jimbo</p>
+
+    <p>Copyright 2021</p>
+  </div>,
+  document.getElementById("root")
+);
